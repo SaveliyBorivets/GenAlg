8f07272e29324a61eba86aa170f1f46f2bb631ee
PetrYakushev <144445892+PetrYakushev@users.noreply.github.com>
2025-06-30 18:40:34 +0300

    PetrYakushev-patch-1

---
+++ item.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+struct Item { // Структура, описывающая предмет
+    float weight; // Вес предмета
+    float value; // Стоимость предмета
+    float unitPrice; // Отношение цены предмета к его весу
+};
